823d4b51d9d33f934d57af6582b43797e9950560
James Warren <admin@trezanik.org>
2014-07-15 07:54:10 +0100

    Partial: RPC + json spirit implementation

--- src/api/Rpc.cc
+++ src/api/Rpc.cc
@@ -8,7 +8,18 @@
 
 
 
+#include <thread>		// hardware_concurrency
+
+#if defined(USING_JSON_SPIRIT_RPC)
+#	include <boost/foreach.hpp>
+#endif
+
+#if defined(_WIN32)
+#	include <Windows.h>
+#endif
+
 #include "Rpc.h"
+#include "utils.h"
 
 
 
@@ -16,16 +27,202 @@ BEGIN_NAMESPACE(APP_NAMESPACE)
 
 
 
-Rpc::Rpc()
+// special function; can't be class member as the return value is for throwing
+json_spirit::Object
+JSONRPCError(
+	ERPCStatus err_code,
+	const std::string& message
+)
+{
+	json_spirit::Object	error;
+	error.push_back(json_spirit::Pair("code", (int64_t) err_code));
+	error.push_back(json_spirit::Pair("message", message));
+	return error;
+}
+
+
+
+
+
+RpcServer::RpcServer()
 {
 
 }
 
 
 
-Rpc::~Rpc()
+RpcServer::~RpcServer()
 {
 
+}
+
+
+
+json_spirit::Value
+RpcServer::GetEnvironmentCoreCount(
+	const json_spirit::Array& params,
+	bool fHelp
+)
+{
+	if ( fHelp || params.size() != 0 )
+	{
+		throw std::runtime_error(
+			"GetEnvironmentCoreCount\n"
+			"Returns the numer of concurrent threads (CPU cores) available"
+		);
+	}
+
+	uint64_t	core_count;
+
+	if (( core_count = std::thread::hardware_concurrency()) == 0 )
+	{
+#if defined(_WIN32)
+		SYSTEM_INFO	si;
+		GetSystemInfo(&si);
+		core_count = si.dwNumberOfProcessors;
+#elif defined (__linux__)
+		core_count = sysconf(_SC_NPROCESSORS_ONLN);
+#elif defined(BSD)
+		int32_t		mib[4];
+		size_t		size = sizeof(core_count);
+
+		mib[0] = CTL_HW;
+		mib[1] = HW_AVAILCPU;
+		sysctl(mib, 2, &core_count, &size, nullptr, 0);
+		if ( core_count < 1 )
+		{
+			mib[1] = HW_NCPU;
+			sysctl(mib, 2, &core_count, &size, nullptr, 0);
+
+			if ( core_count < 1 )
+				core_count = 1;
+		}
+#else
+#	error incomplete for this operating system
+#endif
+	}
+
+	return core_count;
+}
+
+
+
+json_spirit::Value
+RpcServer::GetInterfaceInfo(
+	const json_spirit::Array& params,
+	bool fHelp
+)
+{
+	/** @todo implement RPC interface info acquisition */
+	return 0;
+}
+
+
+
+void 
+RpcServer::TypeCheck(
+	const json_spirit::Array& params,
+	const std::list<json_spirit::Value_type>& expected_types,
+	bool fAllowNull
+) const
+{
+	uint32_t	i = 0;
+	BOOST_FOREACH(json_spirit::Value_type t, expected_types)
+	{
+		if ( params.size() <= i )
+			break;
+
+		const json_spirit::Value& v = params[i];
+		if ( !((v.type() == t) || (fAllowNull && (v.type() == json_spirit::null_type))) )
+		{
+			std::string	err = BUILD_STRING(
+				"Expected type ",
+				value_type_to_string(t),
+				", got ",
+				value_type_to_string(v.type())
+			);
+			
+			throw JSONRPCError(ERPCStatus::UnknownType, err);
+		}
+		i++;
+	}
+}
+
+
+
+void 
+RpcServer::TypeCheck(
+	const json_spirit::Object& o,
+	const std::map<std::string, json_spirit::Value_type>& expected_types,
+	bool fAllowNull
+) const
+{
+	std::string	err;
+
+	BOOST_FOREACH(const PAIRTYPE(std::string, json_spirit::Value_type)& t, expected_types)
+	{
+		const json_spirit::Value& v = find_value(o, t.first);
+		if ( !fAllowNull && v.type() == json_spirit::null_type )
+		{
+			err = BUILD_STRING("Missing ", t.first.c_str());
+
+			throw JSONRPCError(ERPCStatus::UnknownType, err);
+		}
+
+		if ( !((v.type() == t.second) || (fAllowNull && (v.type() == json_spirit::null_type))) )
+		{
+			err = BUILD_STRING(
+				"Expected type ",
+				value_type_to_string(t.second),
+				" for ",
+				t.first.c_str(),
+				", got ",
+				value_type_to_string(v.type())
+			);
+					   
+			throw JSONRPCError(ERPCStatus::UnknownType, err);
+		}
+	}
+}
+
+
+
+
+
+
+static const RpcCommand ApiRpcCommands[] =
+{	//  name                      function                 testmd  unlocked
+	//  ------------------------  -----------------------  ------  --------
+	{ "help", &help, true, true },
+	{ "stop", &stop, true, true },
+};
+
+
+RpcTable::RpcTable()
+{
+	uint32_t	vcidx;
+
+	for ( vcidx = 0; vcidx < (sizeof(ApiRpcCommands) / sizeof(ApiRpcCommands[0])); vcidx++ )
+	{
+		const RpcCommand*	pcmd;
+
+		pcmd = &ApiRpcCommands[vcidx];
+		_cmd_map[pcmd->name] = pcmd;
+	}
+}
+
+
+const RpcCommand*
+RpcTable::operator[] (
+	std::string name
+) const
+{
+	std::map<std::string, const RpcCommand*>::const_iterator it = _cmd_map.find(name);
+
+	if ( it == _cmd_map.end() )
+		return nullptr;
+
+	return (*it).second;
 }
 
 
--- src/api/Rpc.h
+++ src/api/Rpc.h
@@ -3,18 +3,41 @@
 /**
  * @file	Rpc.h
  * @author	James Warren
- * @brief	API RPC
+ * @brief	API RPC Server
+ * @note	If this looks similar to Bitcoin et-al's RPC implementation, it
+ *		is not a coincidence. I've never had to use json spirit or write
+ *		a RPC client or server before, and I'd rather use something that
+ *		works and adapt it then reinvent the wheel - incorrectly. 
+ *		See bitcoinrpc.h for the original.
  */
 
 
 
+#include <string>
+#include <map>
+#include <list>
 
+#if defined(USING_JSON_SPIRIT_RPC)
+#	include <json_spirit/json_spirit_reader_template.h>
+#	include <json_spirit/json_spirit_writer_template.h>
+#	include <json_spirit/json_spirit_utils.h>
+#endif
 
 #include "definitions.h"
 
 
 
 BEGIN_NAMESPACE(APP_NAMESPACE)
+
+
+// the port the server listens on, and the clients must connect to
+#define RPC_PORT		50451
+
+// This is needed because the foreach macro can't get over the comma in pair<t1, t2>
+#define PAIRTYPE(t1, t2)	std::pair<t1, t2>
+
+
+typedef json_spirit::Value(*rpc_function)(const json_spirit::Array& params, bool fHelp);
 
 
 
@@ -26,29 +49,175 @@ BEGIN_NAMESPACE(APP_NAMESPACE)
 enum class ERPCStatus
 {
 	Ok,
-	InvalidParameter,
-	MissingParameter,
-	NotFound,
-	AccessDenied
+	InvalidParameter,	// Supplied parameter invalid (e.g. outside min/max range)
+	MissingParameter,	// Supplied parameter missing (i.e. nullptr)
+	Exception,		// Exception in processing
+	OutOfMemory,		// Memory allocation failed
+	AccessDenied,		// No permissions
+	UnknownType,		// Unrecognized type passed as parameter
+	// json-rpc
+	InvalidRequest,
+	MethodNotFound,
+	InvalidParams,
+	InternalError,
+	ParseError,
 };
 
 
 
 /**
- * RPC server class
+ * 
  */
-class SBI_API Rpc
+class SBI_API RpcCommand
 {
 private:
-	NO_CLASS_ASSIGNMENT(Rpc);
-	NO_CLASS_COPY(Rpc);
+	NO_CLASS_ASSIGNMENT(RpcCommand);
+	// copy is needed for default constructor -> use in tables
 
 protected:
 public:
-	Rpc();
-	~Rpc();
+	std::string	name;
+	rpc_function	actor;
+	bool		allow_test_mode;
+	bool		unlocked;
+};
 
 
+
+
+
+
+
+
+/**
+*
+*/
+class SBI_API RpcTable
+{
+private:
+	NO_CLASS_ASSIGNMENT(RpcTable);
+	NO_CLASS_COPY(RpcTable);
+
+	std::map<std::string, const RpcCommand*>	_cmd_map;
+
+protected:
+public:
+	RpcTable();
+	~RpcTable();
+
+
+	/**
+	 * Accesses a RpcCommand pointer via the associated command name
+	 */
+	const RpcCommand*
+	operator[] (
+		std::string name
+	) const;
+	
+
+	/**
+	 * Retrieves the help string for the associated command name
+	 */
+	std::string
+	Help(
+		std::string name
+	) const;
+
+
+	/**
+	 * Execute a RPC method.
+	 *
+	 * @param method Method to execute
+	 * @param params Array of arguments (JSON objects)
+	 * @returns Result of the call.
+	 * @throws an exception of json_spirit::Value if an error occurs.
+	 */
+	json_spirit::Value
+	Execute(
+		const std::string &method,
+		const json_spirit::Array &params
+	) const;
+};
+
+
+
+/**
+ * RPC server class.
+ *
+ * Interfaces add RPC support for themselves by registering functions into the
+ * server, which adds them to the RPC table. These must be unregistered when the
+ * interface is unloaded.
+ *
+ * Inbuilt commands are public methods within the class (so are pretty much the
+ * libapi RPC commands).
+ */
+class SBI_API RpcServer
+{
+private:
+	NO_CLASS_ASSIGNMENT(RpcServer);
+	NO_CLASS_COPY(RpcServer);
+
+
+	void
+	TypeCheck(
+		const json_spirit::Array& params,
+		const std::list<json_spirit::Value_type>& expected_types,
+		bool fAllowNull
+	) const;
+
+
+	void
+	TypeCheck(
+		const json_spirit::Object& obj,
+		const std::map<std::string, json_spirit::Value_type>& expected_types,
+		bool fAllowNull
+	) const;
+
+
+
+protected:
+public:
+	RpcServer();
+	~RpcServer();
+
+
+	/**
+	 * Adds a new RPC function.
+	 *
+	 * Used by Interfaces so they can enable support for their own
+	 * functionality, and with other third-party options.
+	 *
+	 * @retval ERPCStatus::Ok If the function is added
+	 * @return The relevant ERPCStatus error code on failure
+	 */
+	ERPCStatus
+	AddCallableRPC(
+	);
+
+
+	/**
+	 * Gets the number of CPU execution cores available to the environment.
+	 * 
+	 * Attempts to use C++11 hardware_concurrency by default; if this does
+	 * not return a value, a fallback value obtained using the OS API is
+	 * used.
+	 */
+	json_spirit::Value
+	GetEnvironmentCoreCount(
+		const json_spirit::Array& params,
+		bool fHelp
+	);
+
+
+	/**
+	 * Gets the details for a loaded Interface, such as path, memory address
+	 * and RPC commands, amongst others.
+	 */
+	json_spirit::Value
+	GetInterfaceInfo(
+		const json_spirit::Array& params,
+		bool fHelp
+	);
 
 };
 
